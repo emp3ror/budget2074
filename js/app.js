4da36c24400e7503623a9fcf9ea94ed8b3bd2210
mjt <thapa.manish16@gmail.com>
2017-05-30 21:31:09 +0545

    pie chart added

--- js/app.js
+++ js/app.js
@@ -3,81 +3,68 @@
     var data = [{
         name : 'गरिबीको अन्त्य',
         fund : '2,04,40,32,17',
-        percentage : 15.98
+        y : 15.98
     }, {
         name : 'शून्य भोकमरि',
         fund : '60,74,10,69',
-        percentage : 4.75,
+        y : 4.75,
     }, {
         name : 'स्वस्थ जीवन',
         fund : '32,72,71,52',
-        percentage : 2.56,
+        y : 2.56,
     }, {
         name : 'गणस्तरिय शिक्षा',
         fund : '67,84,60,40',
-        percentage : 5.30,
+        y : 5.30,
     }, {
         name : 'लैङ्गिक  समानता',
         fund : '1,99,01,79',
-        percentage : 0.16,
+        y : 0.16,
     }, {
         name : 'दिगो सफा पानी तथा सरसफाइ सेवा',
         fund : '33,14,65,53',
-        percentage : 2.59,
+        y : 2.59,
     }, {
         name : 'आधुिनक ऊजार्मा पहुँचु',
         fund : '69,32,44,09',
-        percentage : 5.42,
+        y : 5.42,
     }, {
         name : 'समावेशी आथिर्क वृद्धि तथा मर्यादित काम',
         fund : '9,30,60,88',
-        percentage : .73,
+        y : .73,
     }, {
         name : 'उद्योग, नवीन खोज र पूर्वाधार',
         fund : '1,59,34,96,67',
-        percentage : 12.46,
+        y : 12.46,
     }, {
         name : 'असमानता न्यूनीकरण',
         fund : '3,46,47,54',
-        percentage : .27,
+        y : .27,
     }, {
         name : 'दिगो शहर र वस्तीहरू',
         fund : '2,40,97,90,18',
-        percentage : 18.84,
+        y : 18.84,
     }, {
         name : 'दिगो उपभोग तथा उत्पादन',
         fund : '9,43,49',
-        percentage : .01,
+        y : .01,
     }, {
         name : 'जलवायु परिवतर्न अनुकुुलन',
         fund : '3,79,84,65',
-        percentage : .30,
+        y : .30,
     }, {
         name : 'भूसतह स्रोतको उपयोग ',
         fund : '16,31,19,80',
-        percentage : 1.28,
+        y : 1.28,
     }, {
         name : 'शान्तिपूर्ण, न्यायपूर्ण र सशक्त समाज',
         fund : '3,67,90,19,80',
-        percentage : 28.76,
+        y : 28.76,
     }, {
         name : 'दिगो विकासका लागि साझेदारि',
         fund : '7,60,99,35',
-        percentage : .59,
-    }/*, {
-        name : '',
-        fund : '',
-        percentage : ,
-    }, {
-        name : '',
-        fund : '',
-        percentage : ,
-    }, {
-        name : '',
-        fund : '',
-        percentage : ,
+        y : .59,
     }
-*/
 
     ];
 
@@ -92,6 +79,44 @@
     var context = {data : data};
     var html    = template(context);
     $(".text").html(html);
+
+
+    /*charts*/
+    Highcharts.chart('container', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Pie chart - budget 2074/2075'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y:.1f}%</b> <br /> {point.fund}'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.y:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        credits : {
+            enabled : false
+        },
+        series: [{
+            name: 'percentage',
+            colorByPoint: true,
+            data: data
+        }]
+    });
 
 
 
